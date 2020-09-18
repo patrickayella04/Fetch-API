a64b12367bc90f64d7f91dfc32571a7e87a9b8f3
patrickayella04 <60228005+patrickayella04@users.noreply.github.com>
2020-09-19 00:05:58 +0100

    returning text method, to catch it promise and access the text data

--- app.js
+++ app.js
@@ -4,7 +4,15 @@
 document.getElementById('button1').addEventListener('click', getText);
 
 function getText() {
-    fetch('test.txt').then(function (res) {
-        console.log(res);
-    }); // Fetch function returns promises. And when you get a response from a promise you have to use .then()
+    fetch('test.txt').then(function (res) { // Fetch function returns promises. And when you get a response from a promise you have to use .then()
+
+        console.log(res)
+        console.log(res.text())  // Gives us promise result which is 'This is a sample text file' in the console. To get a response from a promise we need to use .then().
+        return res.text(); // We want to return the text method thats in prototype. // res.text() returns a promise so we call another .then() below. 
+    }).then(function (data) { // Here we catch that promise from the return above then we get the data below. 
+        console.log(data);  //we should get 'This is a sample text file'
+    });
+
+    
+
 }
