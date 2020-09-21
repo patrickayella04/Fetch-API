8cca260ce9523147bbeb804bcfd38a4e40222e26
patrickayella04 <60228005+patrickayella04@users.noreply.github.com>
2020-09-21 02:26:23 +0100

    fetching external data

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ document.getElementById('button1').addEventListener('click', getText);
 
 document.getElementById('button2').addEventListener('click', getJson);
 
+document.getElementById('button3').addEventListener('click', getExternal);
+
 
 // Get local text file data
 function getText() {
@@ -38,6 +40,34 @@ function getJson() {
         let output = '';
         data.forEach(function (post) {
             output += `<li>${post.title}</li>`;
+        }); 
+        document.getElementById('output').innerHTML = output;
+
+    })
+        
+        .catch(function (error) {
+        console.log(error);
+    });
+
+    
+
+}
+
+// Get from external API - (api.github.com/users) list of first hundred users on github
+
+function getExternal() {
+    fetch('https://api.github.com/users').then(function (res) { // Fetch function returns promises. And when you get a response from a promise you have to use .then()
+
+        // console.log(res)
+        // console.log(res.text())  // Gives us promise result which is 'This is a sample text file' in the console. To get a response from a promise we need to use .then().
+        return res.json(); // We want to return the text method thats in prototype. // res.text() returns a promise so we call another .then() below. 
+    }).then(function (data) { // Here we catch that promise from the return above then we get the data below. 
+        console.log(data);  //we should get 'This is a sample text file'
+
+        // Here we output the data instead of only console.loging it. It's an array so we'll have to loop through each of them.
+        let output = '';
+        data.forEach(function (user) {
+            output += `<li>${user.login}</li>`;
         }); 
         document.getElementById('output').innerHTML = output;
 
