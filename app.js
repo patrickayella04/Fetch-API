9585f530cf81b787b20a2d030e2cc0637ca0a169
patrickayella04 <60228005+patrickayella04@users.noreply.github.com>
2020-09-23 01:44:53 +0100

    Using arrow functions exampls for cleaner code

--- app.js
+++ app.js
@@ -8,63 +8,121 @@ document.getElementById('button2').addEventListener('click', getJson);
 document.getElementById('button3').addEventListener('click', getExternal);
 
 
+// // Get local text file data
+// function getText() {
+//     fetch('test.txt').then(function (res) { // Fetch function returns promises. And when you get a response from a promise you have to use .then()
+
+//         // console.log(res)
+//         // console.log(res.text())  // Gives us promise result which is 'This is a sample text file' in the console. To get a response from a promise we need to use .then().
+//         return res.text(); // We want to return the text method thats in prototype. // res.text() returns a promise so we call another .then() below. 
+//     }).then(function (data) { // Here we catch that promise from the return above then we get the data below. 
+//         console.log(data);  //we should get 'This is a sample text file'
+//         document.getElementById('output').innerHTML = data; // Here we output the data instead of only console.loging it.
+//     }).catch(function (error) {
+//         console.log(error);
+//     });
+
+    
+
+// }
+
+// USING ARROW FUNCTIONS 
 // Get local text file data
 function getText() {
-    fetch('test.txt').then(function (res) { // Fetch function returns promises. And when you get a response from a promise you have to use .then()
-
-        // console.log(res)
-        // console.log(res.text())  // Gives us promise result which is 'This is a sample text file' in the console. To get a response from a promise we need to use .then().
-        return res.text(); // We want to return the text method thats in prototype. // res.text() returns a promise so we call another .then() below. 
-    }).then(function (data) { // Here we catch that promise from the return above then we get the data below. 
-        console.log(data);  //we should get 'This is a sample text file'
-        document.getElementById('output').innerHTML = data; // Here we output the data instead of only console.loging it.
-    }).catch(function (error) {
-        console.log(error);
-    });
+    fetch('test.txt').then(res => res.text()) 
+        .then(data => { 
+        console.log(data);  
+        document.getElementById('output').innerHTML = data; 
+    }).catch(error => console.log(error));
+    
 
     
 
 }
 
+// // Get local json data
+// function getJson() {
+//     fetch('posts.json').then(function (res) { // Fetch function returns promises. And when you get a response from a promise you have to use .then()
+
+//         // console.log(res)
+//         // console.log(res.text())  // Gives us promise result which is 'This is a sample text file' in the console. To get a response from a promise we need to use .then().
+//         return res.json(); // We want to return the text method thats in prototype. // res.text() returns a promise so we call another .then() below. 
+//     }).then(function (data) { // Here we catch that promise from the return above then we get the data below. 
+//         console.log(data);  //we should get 'This is a sample text file'
+
+//         // Here we output the data instead of only console.loging it. It's an array so we'll have to loop through each of them.
+//         let output = '';
+//         data.forEach(function (post) {
+//             output += `<li>${post.title}</li>`;
+//         }); 
+//         document.getElementById('output').innerHTML = output;
+
+//     })
+        
+//         .catch(function (error) {
+//         console.log(error);
+//     });
+
+    
+
+// }
+
+// USING ARROW FUNCTIONS 
 // Get local json data
 function getJson() {
-    fetch('posts.json').then(function (res) { // Fetch function returns promises. And when you get a response from a promise you have to use .then()
+    fetch('posts.json')
+        .then(res => res.json())
+        .then(data => { 
+        console.log(data); 
 
-        // console.log(res)
-        // console.log(res.text())  // Gives us promise result which is 'This is a sample text file' in the console. To get a response from a promise we need to use .then().
-        return res.json(); // We want to return the text method thats in prototype. // res.text() returns a promise so we call another .then() below. 
-    }).then(function (data) { // Here we catch that promise from the return above then we get the data below. 
-        console.log(data);  //we should get 'This is a sample text file'
-
-        // Here we output the data instead of only console.loging it. It's an array so we'll have to loop through each of them.
+        
         let output = '';
         data.forEach(function (post) {
             output += `<li>${post.title}</li>`;
         }); 
         document.getElementById('output').innerHTML = output;
 
-    })
-        
-        .catch(function (error) {
-        console.log(error);
-    });
+    }).catch(error => console.log(error));
 
     
 
 }
 
+// // Get from external API - (api.github.com/users) list of first hundred users on github
+
+// function getExternal() {
+//     fetch('https://api.github.com/users').then(function (res) { // Fetch function returns promises. And when you get a response from a promise you have to use .then()
+
+//         // console.log(res)
+//         // console.log(res.text())  // Gives us promise result which is 'This is a sample text file' in the console. To get a response from a promise we need to use .then().
+//         return res.json(); // We want to return the text method thats in prototype. // res.text() returns a promise so we call another .then() below. 
+//     }).then(function (data) { // Here we catch that promise from the return above then we get the data below. 
+//         console.log(data);  //we should get 'This is a sample text file'
+
+//         // Here we output the data instead of only console.loging it. It's an array so we'll have to loop through each of them.
+//         let output = '';
+//         data.forEach(function (user) {
+//             output += `<li>${user.login}</li>`;
+//         }); 
+//         document.getElementById('output').innerHTML = output;
+
+//     })
+        
+//         .catch(function (error) {
+//         console.log(error);
+//     });
+
+    
+
+// }
+
 // Get from external API - (api.github.com/users) list of first hundred users on github
 
 function getExternal() {
-    fetch('https://api.github.com/users').then(function (res) { // Fetch function returns promises. And when you get a response from a promise you have to use .then()
+    fetch('https://api.github.com/users').then(res => res.json()).then(data => { 
+        console.log(data);  
 
-        // console.log(res)
-        // console.log(res.text())  // Gives us promise result which is 'This is a sample text file' in the console. To get a response from a promise we need to use .then().
-        return res.json(); // We want to return the text method thats in prototype. // res.text() returns a promise so we call another .then() below. 
-    }).then(function (data) { // Here we catch that promise from the return above then we get the data below. 
-        console.log(data);  //we should get 'This is a sample text file'
-
-        // Here we output the data instead of only console.loging it. It's an array so we'll have to loop through each of them.
+        
         let output = '';
         data.forEach(function (user) {
             output += `<li>${user.login}</li>`;
@@ -73,9 +131,7 @@ function getExternal() {
 
     })
         
-        .catch(function (error) {
-        console.log(error);
-    });
+        .catch(error => console.log(error));
 
     
 
